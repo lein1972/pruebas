642b88716804d7018e8fb5424e90757c386aeb7f
lgalan <lein.galan@sine95.com>
2025-12-24 14:20:48 -0400

    feat: base para ejercicio de conflicto

--- operaciones.ts
+++ operaciones.ts
@@ -1,39 +1,4 @@
-// Función para sumar dos números
-const sumar = (a: number, b: number): number => {
-
-    console.log("Azul: Sumando valores...");
-    return a + b;
+// Archivo de práctica: Conflictos
+const saludar = () => {
+    console.log("Hola estudiantes, listos para el break?");
 };
-
-// Función para restar dos números
-const restar = (a: number, b: number): number => {
-    return a - b;
-};
-
-// Función para multiplicar
-const multiplicar = (a: number, b: number): number => {
-    return a * b;
-};
-
-// Función para dividir
-const dividir = (a: number, b: number): number => {
-    if (b === 0) {
-        console.error("No se puede dividir por cero");
-        return 0;
-    }
-    return a / b;
-};
-
-// Función para elevar un número a una potencia
-const potencia = (base: number, exponente: number): number => {
-    return Math.pow(base, exponente);
-};
-
-console.log("Potencia:", potencia(2, 3)); // Debería dar 8
-
-console.log("Multiplicación:", multiplicar(10, 5));
-console.log("División:", dividir(10, 5));
-
-// Pruebas rápidas
-console.log("La suma total es igual a:", sumar(10, 5));
-console.log("RESTA A:", restar(10,5));
