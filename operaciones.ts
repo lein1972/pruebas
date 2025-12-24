d77a85d5a495a307336bedd8b5bdcc81b6037c9a
lgalan <lein.galan@sine95.com>
2025-12-24 10:23:39 -0400

    se añadio: multiplicacion, division

--- operaciones.ts
+++ operaciones.ts
@@ -8,6 +8,23 @@ const restar = (a: number, b: number): number => {
     return a - b;
 };
 
+// Función para multiplicar
+const multiplicar = (a: number, b: number): number => {
+    return a * b;
+};
+
+// Función para dividir
+const dividir = (a: number, b: number): number => {
+    if (b === 0) {
+        console.error("No se puede dividir por cero");
+        return 0;
+    }
+    return a / b;
+};
+
+console.log("Multiplicación:", multiplicar(10, 5));
+console.log("División:", dividir(10, 5));
+
 // Pruebas rápidas
 console.log("Suma:", sumar(10, 5));
 console.log("Resta:", restar(10, 5));
